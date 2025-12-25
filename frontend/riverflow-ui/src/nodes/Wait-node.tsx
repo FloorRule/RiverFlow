b07f92386ded15d3924bd2c610eba031423e5393
Orel Tzur <56762076+FloorRule@users.noreply.github.com>
2025-12-25 16:29:01 +0200

    Wait/Delay node added

--- frontend/riverflow-ui/src/nodes/Wait-node.tsx
+++ frontend/riverflow-ui/src/nodes/Wait-node.tsx
@@ -2,14 +2,14 @@ import { Position, Handle, type NodeProps , type Node} from "@xyflow/react";
 import { Settings } from 'lucide-react';
 
 //! Types
-export type ScriptNodeData = {
+export type WaitNodeData = {
   onOpen: (id: string) => void;
-  pythonScript: string;
+  delay: string;
 };
 
-//! Script Node
+//! Wait Node
 
-export function ScriptNode({ id, data }: NodeProps<Node<ScriptNodeData>['data']>) {
+export function WaitNode({ id, data }: NodeProps<Node<WaitNodeData>['data']>) {
   return (
     <div>
       <button
@@ -18,21 +18,21 @@ export function ScriptNode({ id, data }: NodeProps<Node<ScriptNodeData>['data']>
       >
         <Settings size={14} />
       </button>
-      <div className="relative flex h-20 w-20 items-center justify-center rounded-full border-2 border-yellow-500 bg-card shadow-md">
-        <span className="text-sm font-semibold text-yellow-600 dark:text-yellow-300">
-          Python
+      <div className="relative flex h-20 w-20 items-center justify-center rounded-full border-2 border-pink-500 bg-card shadow-md">
+        <span className="text-sm font-semibold text-pink-600 dark:text-pink-300">
+          Wait
         </span>
 
         <Handle
           type="target"
           position={Position.Left}
-          className="!h-3 !w-3 !bg-yellow-500"
+          className="!h-3 !w-3 !bg-pink-500"
         />
 
         <Handle
           type="source"
           position={Position.Right}
-          className="!h-3 !w-3 !bg-yellow-500"
+          className="!h-3 !w-3 !bg-pink-500"
         />
 
       </div>
