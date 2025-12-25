f699efb2844792908f257d9c44a369d1ccd43dba
Orel Tzur <56762076+FloorRule@users.noreply.github.com>
2025-12-25 15:55:37 +0200

    script node added

---
+++ frontend/riverflow-ui/src/nodes/Script-node.tsx
@@ -0,0 +1,41 @@
+import { Position, Handle, type NodeProps , type Node} from "@xyflow/react";
+import { Settings } from 'lucide-react';
+
+//! Types
+export type ScriptNodeData = {
+  onOpen: (id: string) => void;
+  pythonScript: string;
+};
+
+//! Condition Node
+
+export function ScriptNode({ id, data }: NodeProps<Node<ScriptNodeData>['data']>) {
+  return (
+    <div>
+      <button
+        onClick={() => data.onOpen(id)}
+        className="nodrag flex h-8 w-8 items-center justify-center rounded-full border border-border bg-card text-foreground transition hover:bg-muted"
+      >
+        <Settings size={14} />
+      </button>
+      <div className="relative flex h-20 w-20 items-center justify-center rounded-full border-2 border-yellow-500 bg-card shadow-md">
+        <span className="text-sm font-semibold text-yellow-600 dark:text-yellow-300">
+          Python
+        </span>
+
+        <Handle
+          type="target"
+          position={Position.Left}
+          className="!h-3 !w-3 !bg-yellow-500"
+        />
+
+        <Handle
+          type="source"
+          position={Position.Right}
+          className="!h-3 !w-3 !bg-yellow-500"
+        />
+
+      </div>
+    </div>
+  );
+}
