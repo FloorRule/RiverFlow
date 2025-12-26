8d2dc30863bc131dcb3e6a4a08f549125f159882
Orel Tzur <56762076+FloorRule@users.noreply.github.com>
2025-12-26 23:54:06 +0200

    RestAPI setup + JSON scheme

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+import threading
+import time
+import uvicorn
+from API_server import app
+from db.init_db_script import init_db
+
+def start_fastapi():
+    #init_db()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+print("FastAPI are running...")
+start_fastapi()
