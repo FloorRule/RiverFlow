ad3ac93d0ad71b8e78c74cd6172e9a1294df50bc
Orel Tzur <56762076+FloorRule@users.noreply.github.com>
2025-12-27 20:49:29 +0200

    node classes

---
+++ backend/nodes/scriptNode.py
@@ -0,0 +1,9 @@
+from backend.nodes import BaseNode
+
+class scriptNode(BaseNode):
+  def __init__(self, id, type, position, config):
+    super().__init__(id, type, position)
+    self.config = config
+
+  def execute_node(self, context):
+    print(self.config)
